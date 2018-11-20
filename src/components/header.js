d594906cdde27039cb141ce3046fa0bf469de1c1
indubc <35763642+indubc@users.noreply.github.com>
2018-11-20 13:44:37 +0530

    added css for header

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import{Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-const Header = ()=>{
-    return(
-        <header>
-
-<Link to="/">Music Db</Link>
-
+const Header = () => {
+    return (
+        <header className="header">
+            <Link to="/">Music Db
+            </Link>
+            <span></span>
         </header>
+
     )
 }
 export default Header;
